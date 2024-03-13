44b56e2ec1524ca4232d9dda95925f252e2d2e64
Epik95mkw <epik955@gmail.com>
2024-03-13 17:15:36 -0400

    get best splits function

--- api/chadsoft.py
+++ api/chadsoft.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 
 API_URL = 'https://tt.chadsoft.co.uk'
 GHOST_URL = 'https://chadsoft.co.uk/time-trials'
@@ -29,3 +30,43 @@ def get_leaderboard(slot: str, sha1: str, category: int, is_flap: bool, is_200: 
 
 def get_bkt_url(leaderboard_json):
     return GHOST_URL + leaderboard_json['ghosts'][0]['_links']['item']['href'][0:-4] + 'html'
+
+
+def _get_all_best_splits():
+    out_dir = r'C:/Users/epik9/Desktop/chadsoft_data/raw/'
+    start_index = 27
+    ghosts_per_track = 30
+
+    leaderboards = all_leaderboards()['leaderboards']
+    leaderboards = [track for track in leaderboards if track.setdefault('categoryId', 0) == 0]
+
+    for i, track in enumerate(leaderboards):
+        if i < start_index:
+            continue
+
+        name = track['name']
+        out = {
+            'name': name,
+            'ghosts': []
+        }
+        track_url = API_URL + track['_links']['item']['href']
+
+        print(f'({i}) Loading leaderboard for {name}...')
+        lb = openurl(track_url, 'utf-8-sig').json()
+        print('Loaded.')
+
+        for j, ghost in enumerate(lb['ghosts'][:ghosts_per_track]):
+            ghost_url = API_URL + ghost['_links']['item']['href']
+            print(f'    {j + 1}: {ghost_url}')
+            ghost_data = openurl(ghost_url, 'utf-8-sig').json()
+            out['ghosts'].append(ghost_data)
+            time.sleep(1)
+
+        with open(out_dir + name + '.json', 'w') as f:
+            f.write(json.dumps(out, indent=4))
+
+        time.sleep(40)
+
+
+if __name__ == '__main__':
+    _get_all_best_splits()
